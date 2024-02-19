99650b4b7cbf5a6be4d4d0586f42b4d8ca1d705b
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-02-19 09:03:37 +0100

    Añadir feeds de prueba al usuario editor

--- laravel/database/seeders/EditorUserSeeder.php
+++ laravel/database/seeders/EditorUserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Feed;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -23,5 +24,29 @@ class EditorUserSeeder extends Seeder
         ]);
 
         $user->assignRole('editor');
+
+        Feed::create([
+            'title' => 'El País',
+            'url' => 'https://feeds.elpais.com/mrss-s/pages/ep/site/elpais.com/section/ultimas-noticias/portada',
+            'user_id' => $user->id,
+        ]);
+
+        Feed::create([
+            'title' => 'Tecnalia',
+            'url' => 'https://cms.tecnalia.com/feed/?post_type=news&lang=es',
+            'user_id' => $user->id,
+        ]);
+
+        Feed::create([
+            'title' => 'Xataka',
+            'url' => 'https://feeds.weblogssl.com/xataka2',
+            'user_id' => $user->id,
+        ]);
+
+        Feed::create([
+            'title' => 'Enpresa Digitala',
+            'url' => 'https://enpresadigitala.spri.eus/es/rss/',
+            'user_id' => $user->id,
+        ]);
     }
 }
