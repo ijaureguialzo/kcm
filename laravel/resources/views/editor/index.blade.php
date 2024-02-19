870a410a3575da3c585b0b8a67b4d75faa35542a
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-02-19 08:55:52 +0100

    Visualización de feed mejorada

--- laravel/resources/views/editor/index.blade.php
+++ laravel/resources/views/editor/index.blade.php
@@ -10,39 +10,38 @@
         <table class="table table-hover">
             <thead>
             <tr class="table-dark">
-                <th>{{ __('Item') }}</th>
-                <th>{{ __('Link') }}</th>
-                <th>{{ __('Actions') }}</th>
+                <th class="small">{{ __('Item') }}</th>
+                <th class="small">{{ __('Actions') }}</th>
             </tr>
             </thead>
-            <tbody class="align-middle">
+            <tbody class="align-middle table-group-divider">
             @foreach ($rss->get_items() as $item)
                 <tr>
                     <td>
                         <div>
-                            <p class="fw-bold small m-0">{{ $item->get_title() }}</p>
-                            <p class="small m-0">{{ $item->get_description() }}</p>
+                            <p class="fw-bold small m-0">{{ Str::of($item->get_title())->stripTags()->limit(255) }}</p>
+                            <p class="small m-0">{{ Str::of($item->get_description())->stripTags()->limit(255) }}</p>
                         </div>
                     </td>
                     <td>
-                        <a href="{{ $item->get_link() }}"
-                           target="_blank"
-                           title="{{ __('View original entry') }}"
-                           class="btn btn-sm btn-secondary me-2" role="button">
-                            <i class="bi bi-link"></i>
-                        </a>
-                    </td>
-                    <td>
-                        <a href="#"
-                           title="{{ __('Add to bucket') }}"
-                           class="btn btn-sm btn-primary me-2" role="button">
-                            <i class="bi bi-plus-lg"></i>
-                        </a>
+                        <div class="d-flex">
+                            <a href="{{ $item->get_link() }}"
+                               target="_blank"
+                               title="{{ __('View original entry') }}"
+                               class="btn btn-sm btn-secondary me-2" role="button">
+                                <i class="bi bi-link"></i>
+                            </a>
+                            <a href="#"
+                               title="{{ __('Add to bucket') }}"
+                               class="btn btn-sm btn-primary me-2" role="button">
+                                <i class="bi bi-plus-lg"></i>
+                            </a>
+                        </div>
                     </td>
                 </tr>
             @endforeach
             </tbody>
-            <tfoot>
+            <tfoot class="table-group-divider">
             <tr>
                 <th colspan="5" class="border-0">{{ __('Total') }}: {{ count($rss->get_items()) }}</th>
             </tr>
