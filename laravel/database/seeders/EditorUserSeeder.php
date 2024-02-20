e42a6c4169c0a4ab6924f108cdee2154e69d42f3
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-02-21 00:43:30 +0100

    Optimización de imports

--- laravel/database/seeders/EditorUserSeeder.php
+++ laravel/database/seeders/EditorUserSeeder.php
@@ -4,10 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Feed;
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Spatie\Permission\Models\Permission;
-use Spatie\Permission\Models\Role;
 
 class EditorUserSeeder extends Seeder
 {
