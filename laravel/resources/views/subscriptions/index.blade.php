ae2f8f064628ad3e7069e7be7233a7aaa5df277b
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-03-08 12:10:36 +0100

    Tarjeta mejorada

--- laravel/resources/views/subscriptions/index.blade.php
+++ laravel/resources/views/subscriptions/index.blade.php
@@ -14,20 +14,22 @@
         @foreach ($repositories as $key => $repository)
             <div class="col">
                 <div class="card h-100">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $repository->title }}</h5>
-                        <p class="card-text">{{ $repository->description }}</p>
-                        <p class="card-text">{{ __('Curated by') }}: {{ $repository->owner->name }}</p>
-                    </div>
-                    <div class="card-body text-end">
-                        <a href="#"
-                           class="btn btn-sm btn-primary me-2" role="button">
-                            <i class="bi bi-eye"></i><span class="ms-2">{{ __('View compilations') }}</span>
-                        </a>
+                    <div class="card-body d-flex flex-column justify-content-between">
+                        <div>
+                            <h5 class="card-title">{{ $repository->title }}</h5>
+                            <p class="card-text">{{ $repository->description }}</p>
+                            <p class="card-text small">{{ __('Curated by') }}: {{ $repository->owner->name }}</p>
+                        </div>
+                        <div class="text-end">
+                            <a href="#"
+                               class="btn btn-sm btn-primary" role="button">
+                                <i class="bi bi-eye"></i><span class="ms-2">{{ __('View compilations') }}</span>
+                            </a>
+                        </div>
                     </div>
                     <div class="card-footer">
                         <small class="text-body-secondary">
-                            {{ __(':number subscribers.', ['number' => $repository->subscribers->count()]) }}
+                            {{ __(':number subscribers', ['number' => $repository->subscribers->count()]) }}
                         </small>
                     </div>
                 </div>
