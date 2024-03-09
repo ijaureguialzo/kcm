4800f6e354638f91f71f28db2b03f9b1a01976e1
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-03-09 10:10:52 +0100

    Guardar el locale del usuario en el perfil

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -69,4 +69,9 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         return $this->hasManyThrough(Compilation::class, Repository::class);
     }
+
+    public function profile()
+    {
+        return $this->hasOne(UserProfile::class);
+    }
 }
